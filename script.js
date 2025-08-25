5de014a6147ad44c32ce9a328a098cbe91cd926a
Gideon Onyegbula <gigi.onyegbula.2007@gmail.com>
2025-08-25 16:40:26 -0700

    Prompt Course for Quiz

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 import { Quiz } from "./quiz.js";
 
+const options = document.getElementById("courseList");
+const chooseBtn = document.getElementById("chooseButton");
 let course = "./courses/biology/TypeA.json";
+
+
+options.addEventListener('click', () => {
+    options[0].disabled = true;
+});
+chooseBtn.addEventListener('click', () => {
+    console.log(options.value);
+});
+
+function chooseCourse() {
+    
+}
+
+
 Quiz(course);
