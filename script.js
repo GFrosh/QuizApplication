dfa266ae09cd28be3193bc89d1b24dd08f6ce807
Gideon Onyegbula <gigi.onyegbula.2007@gmail.com>
2025-08-27 01:10:20 -0700

    New Commit

--- script.js
+++ script.js
@@ -2,19 +2,29 @@ import { Quiz } from "./quiz.js";
 
 const options = document.getElementById("courseList");
 const chooseBtn = document.getElementById("chooseButton");
-let course = "./courses/biology/TypeA.json";
+const chooseSection = document.getElementById("selectCourse");
+const quizSection = document.getElementById("quiz-container");
 
+let typeIndex = 0;
+let course = {
+    biology: ["./courses/biology/TypeA.json", "./courses/biology/TypeB.json", "./courses/biology/TypeC.json", "./courses/biology/TypeD.json", "./courses/biology/TypeE.json"],
+    ins: ["I think it works", "Or not"]
+}
+
+function chooseCourse() {
+    quizSection.style.display = "none";
+    console.log(chooseSection);
+
+}
 
 options.addEventListener('click', () => {
     options[0].disabled = true;
 });
 chooseBtn.addEventListener('click', () => {
     console.log(options.value);
+    console.log(course);
+    chooseCourse();
 });
 
-function chooseCourse() {
-    
-}
 
-
-Quiz(course);
+Quiz(course.biology[0]);
