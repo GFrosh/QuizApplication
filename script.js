e76ee52c6140c909e4db2a7787222ef34cbd2246
Gideon Onyegbula <gigi.onyegbula.2007@gmail.com>
2025-08-29 01:15:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,6 @@ const quizSection = document.getElementById("quiz-container");
 const selectedCourse = document.getElementById("selectedCourse");
 
 let type = "TypeA";
-let course = {
-    biology: {
-        typeA: "./courses/biology/TypeA.json",
-        typeB: "./courses/biology/TypeB.json",
-        typeC: "./courses/biology/TypeC.json",
-        typeD: "./courses/biology/TypeD.json",
-        typeE: "./courses/biology/TypeE.json"
-    },
-    ins: {
-        typeA: "I think it works",
-        typeB: "Or not"
-    }
-}
 
 function toggleSection(sectionA, sectionB) {
     sectionA.style.display = "none";
